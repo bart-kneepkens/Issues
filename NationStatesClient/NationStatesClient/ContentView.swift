4268e099da9a4316a2da7fae4ccc2f5c8989c8b8
Bart Kneepkens <bkneepkens@msn.com>
2020-10-07 23:48:47 +0200

    Initialo commiterino

---
+++ NationStatesClient/NationStatesClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NationStatesClient
+//
+//  Created by Bart Kneepkens on 07/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NationStatesClient/NationStatesClient/NationStatesClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationStatesClientApp.swift
+//  NationStatesClient
+//
+//  Created by Bart Kneepkens on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NationStatesClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
