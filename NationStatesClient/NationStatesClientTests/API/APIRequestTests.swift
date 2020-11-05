33ed53b1f885b224735cf1c24b1228cb09e01af4
Bart Kneepkens <bkneepkens@msn.com>
2020-11-05 20:21:47 +0100

    Some APIRequest tests

--- NationStatesClient/NationStatesClientTests/API/APIRequestTests.swift
+++ NationStatesClient/NationStatesClientTests/API/APIRequestTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Combine
 @testable import NationStatesClient
 
 fileprivate class MockedSecureStorage: SecureStorage {
@@ -35,21 +36,9 @@ private func authenticationContainerWith(nationName: String = "",
     return container
 }
 
-private func getPasswordHeader(_ request: URLRequest) -> String? {
-    return request.value(forHTTPHeaderField: "X-Password")
-}
-
-private func getPinHeader(_ request: URLRequest) -> String? {
-    return request.value(forHTTPHeaderField: "X-pin")
-}
-
-private func getAutologinHeader(_ request: URLRequest) -> String? {
-    return request.value(forHTTPHeaderField: "X-autologin")
-}
+private let mockURL = URL(string: "test_url")!
 
 class APIRequest_AuthenticationHeaders_Tests: XCTestCase {
-    let mockURL = URL(string: "test_url")!
-    
     func testPasswordHeader() throws {
         let container = authenticationContainerWith(nationName: "test_nationName", password: "test_password")
         let request = APIRequest(url: mockURL, authenticationContainer: container).authenticated
@@ -91,5 +80,76 @@ class APIRequest_AuthenticationHeaders_Tests: XCTestCase {
         XCTAssertEqual(getAutologinHeader(request), "test_autologin")
         XCTAssertNil(getPinHeader(request))
         XCTAssertNil(getPasswordHeader(request))
+    }
+    
+    private func getPasswordHeader(_ request: URLRequest) -> String? {
+        return request.value(forHTTPHeaderField: "X-Password")
+    }
+    
+    private func getPinHeader(_ request: URLRequest) -> String? {
+        return request.value(forHTTPHeaderField: "X-pin")
+    }
+    
+    private func getAutologinHeader(_ request: URLRequest) -> String? {
+        return request.value(forHTTPHeaderField: "X-autologin")
+    }
+}
+
+class APIRequest_Publisher_HTTP_Errors_Tests: XCTestCase {
+    class MockedSession: NetworkSession {
+        var statusCode: Int
+        
+        init(statusCode: Int) {
+            self.statusCode = statusCode
+        }
+        
+        func publisher(for request: URLRequest) -> AnyPublisher<DataResponse, URLError> {
+            let httpResponse = HTTPURLResponse(url: mockURL, statusCode: self.statusCode, httpVersion: nil, headerFields: nil)!
+            return Just((data: Data(), response: httpResponse))
+                .mapError({ failure -> URLError in })
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    func testHTTPError403() throws {
+        let container = authenticationContainerWith(nationName: "test_nationName", password: "test_password")
+        let expectation = self.expectation(description: "throws APIError .unauthorized in case of HTTP error 403")
+        
+        let _ = APIRequest(url: mockURL, authenticationContainer: container, session: MockedSession(statusCode: 403))
+            .publisher
+            .sink { completion in
+                switch completion {
+                case .failure(let apiError):
+                    switch apiError {
+                    case .unauthorized:
+                        expectation.fulfill()
+                    default: break
+                    }
+                default: break
+                }
+            } receiveValue: { _ in }
+        
+        self.waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testHTTPError409() throws {
+        let container = authenticationContainerWith(nationName: "test_nationName", password: "test_password")
+        let expectation = self.expectation(description: "throws APIError .conflict in case of HTTP error 409")
+        
+        let _ = APIRequest(url: mockURL, authenticationContainer: container, session: MockedSession(statusCode: 409))
+            .publisher
+            .sink { completion in
+                switch completion {
+                case .failure(let apiError):
+                    switch apiError {
+                    case .conflict:
+                        expectation.fulfill()
+                    default: break
+                    }
+                default: break
+                }
+            } receiveValue: { _ in }
+        
+        self.waitForExpectations(timeout: 1, handler: nil)
     }
 }
